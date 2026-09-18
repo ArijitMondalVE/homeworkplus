94c334afb9312103d47634178bbd6cb8242936a7
ArijitMondalVE <arijitmondal@virtualemployee.com>
2026-09-18 16:48:56 +0530

    Update-deployment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://homeworkplus.onrender.com/api/v1',
-  wsUrl: 'wss://homeworkplus.onrender.com',
+  apiUrl: 'https://homeworkplus-1.onrender.com/api/v1',
+  wsUrl: 'wss://homeworkplus-1.onrender.com',
 };
